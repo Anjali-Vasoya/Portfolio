d34db0c5b434b9abe3a2c405eede4ebca83437c3
rajatgajera <rajatgajera2212@gmail.com>
2025-12-10 14:48:55 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+public class HomeController : Controller
+{
+    public IActionResult Index() => View();
+    public IActionResult About() => View();
+    public IActionResult Skills() => View();
+    public IActionResult Projects() => View();
+    public IActionResult Contact() => View();
+    public IActionResult Experience() => View();
+
+    [HttpPost]
+    public IActionResult SendMessage(string Name, string Email,string Subject, string Message)
+    {
+        //var mail = new MailMessage();
+        //mail.To.Add("anjalivasoya12@gmail.com");
+        //mail.From = new MailAddress("your-email@gmail.com");
+        //mail.Subject = "Portfolio Contact Message";
+        //mail.Body = $"Name: {Name}\nEmail: {Email}\nMessage: {Message}";
+
+        //var smtp = new SmtpClient("smtp.gmail.com", 587)
+        //{
+        //    Credentials = new NetworkCredential("your-email@gmail.com", "your-app-password"),
+        //    EnableSsl = true
+        //};
+
+        //smtp.Send(mail);
+
+        //ViewBag.Success = "Message Sent Successfully!";
+        //return View("Contact");
+        return RedirectToAction("Contact");
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,16 @@
+﻿# Base image
+FROM mcr.microsoft.com/dotnet/aspnet:7.0 AS base
+WORKDIR /app
+EXPOSE 7132
+
+# Build stage
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY . .
+RUN dotnet publish -c Release -o /app
+
+# Final stage
+FROM base AS final
+WORKDIR /app
+COPY --from=build /app .
+ENTRYPOINT ["dotnet", "Website.dll"]
